55a5e9c8864198b1c01ff5c489ea5cecc762a6cf
Takayuki YAMAGUCHI <d@ytak.info>
2014-01-31 09:44:56 +0900

    Fix sample

--- examples/sample_command.rb
+++ examples/sample_command.rb
@@ -4,18 +4,22 @@ require "subcommand_optparse"
 options = {}
 
 parser = SubCmdOptParser.new(:help_command => true) do |sc|
+  sc.program_name = "sample_command.rb"
+  sc.version = "0.0.1"
+  sc.release = "hello world"
+
   sc.global_option do |opt|
     opt.on("--global-option", "Banner of global option") do |v|
       options[:global_option] = true
     end
   end
-  sc.subcommand("foo", "Banner of foo") do |opt|
+  sc.subcommand("foo", "Description of foo") do |opt|
     opt.on("--foo-option", "Foo option") do |v|
       options[:foo] = true
     end
   end
   sc.subcommand("bar") do |opt|
-    opt.on("--bar-option", "Bar option") do |v|
+    opt.on("--bar-option", "Description of bar") do |v|
       options[:bar] = true
     end
   end
